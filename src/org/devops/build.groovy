3354950142d0cd76a23732859dea16bd93780de4
PhilosophyYuan <70202145+PhilosophyYuan@users.noreply.github.com>
2020-09-30 00:01:33 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -1,4 +1,4 @@
-package org.devops
+packge org.devops
 def Build(buildType,buildShell){
   def buildTools = ["mvn":"M2","ant":ANT,"gradle":"GRADLE","node":NODEJS]
   println("当前选择的构建类型为${buildType}")
